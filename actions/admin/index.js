8862f0f95054eea490a28a0b2e46f04659922ade
oldDriver <fktrctq>
2015-03-20 19:37:36 +0200

    small step forward

--- actions/admin/index.js
+++ actions/admin/index.js
@@ -1,3 +1,11 @@
+var User = require('models/user');
+
 exports.get = function(req, res) {
-    res.render('admin/index');
+    User.all({order: "nickname", limit: 10}, function(err, results){
+        if (err) throw err;
+        //console.log(results);
+        res.render('admin/index', {
+            users: results
+        });
+    });
 };
--- actions/index.js
+++ actions/index.js
@@ -1,12 +1,3 @@
-///var User = require('models/user');
 exports.get = function(req, res) {
     res.render('public/index');
-    //User.all({order: "nickname", limit: 10}, function(err, results){
-    //    if (err) throw err;
-    //    //console.log(results);
-    //    res.render('index', {
-    //        users: results
-    //    });
-    //});
-
 };
